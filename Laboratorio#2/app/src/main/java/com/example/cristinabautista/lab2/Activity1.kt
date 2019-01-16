e683226fe342ce9e76e746ca1f49f0b88700dcf4
Cristina B <bau161260@uvg.edu.gt>
2019-01-16 16:17:24 -0600

    Primer intento de ArrayAdapter

--- Laboratorio#2/app/src/main/java/com/example/cristinabautista/lab2/Activity1.kt
+++ Laboratorio#2/app/src/main/java/com/example/cristinabautista/lab2/Activity1.kt
@@ -3,13 +3,28 @@ package com.example.cristinabautista.lab2
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 
 class Activity1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
+        val lista: ListView = findViewById(R.id.foodLIst)
+
+        var foodList = ArrayList<String>()
+        foodList.add("Helado")
+        foodList.add("Pastel")
+        foodList.add("Pie")
+        foodList.add("Donas")
+        foodList.add("Flan")
+
+        val ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, foodList)
+
+        lista.adapter = ArrayAdapter
 
         val buttonShowHome = findViewById<Button>(R.id.button3)
         buttonShowHome.setOnClickListener{val intent = Intent(this, Activity::class.java)
--- Laboratorio#2/app/src/main/java/com/example/cristinabautista/lab2/MyApplication.kt
+++ Laboratorio#2/app/src/main/java/com/example/cristinabautista/lab2/MyApplication.kt
@@ -1,4 +1,8 @@
 package com.example.cristinabautista.lab2
 
+import com.example.oscar.myfirstapp.MenuOrder
+
 class MyApplication {
+    private val order: MenuOrder = MenuOrderImp()
+
 }
