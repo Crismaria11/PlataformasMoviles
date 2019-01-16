841c4f11639b50fd1522f774fd7de887105d6418
Cristina B <bau161260@uvg.edu.gt>
2019-01-16 07:21:12 -0600

    Lab#2

---
+++ Laboratorio#2/app/src/main/java/com/example/cristinabautista/lab2/MainActivityRestaurante.kt
@@ -0,0 +1,12 @@
+package com.example.cristinabautista.lab2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRestaurante : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_restaurante)
+    }
+}
